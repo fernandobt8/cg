012664c801191f6c1f9af179dd31169142455edf
Alisson Granemann <alissongranemannabreu@gmail.com>
2014-05-15 21:52:50 -0300

    correcao

--- src/transformacao/tipos/rotacao/RotacaoView.cpp
+++ src/transformacao/tipos/rotacao/RotacaoView.cpp
@@ -11,79 +11,69 @@ RotacaoView::RotacaoView(OnAdicionarTipoTransformacaoEvent* event) :
 		QWidget() {
 	this->event = event;
 	frame = new QFrame(this);
-	frame->setGeometry(QRect(20, 30, 320, 100));
+	frame->setGeometry(QRect(20, 30, 320, 70));
 	frame->setFrameShape(QFrame::WinPanel);
 	frame->setFrameShadow(QFrame::Sunken);
 	QLabel *operacoesLabel = new QLabel(this);
 	operacoesLabel->setGeometry(QRect(30, 10, 80, 20));
 	operacoesLabel->setText(QString::fromUtf8("Operações"));
-	origemButton = new QRadioButton(this);
-	origemButton->setObjectName(QString::fromUtf8("origemButton"));
-	origemButton->setGeometry(QRect(40, 40, 241, 22));
-	origemButton->setText(QString::fromUtf8("Rotacionar sobre a origem"));
 	centroButton = new QRadioButton(this);
 	centroButton->setObjectName(QString::fromUtf8("centroButton"));
-	centroButton->setGeometry(QRect(40, 70, 281, 22));
+	centroButton->setGeometry(QRect(40, 40, 300, 20));
 	centroButton->setText(
 			QString::fromUtf8("Rotacionar sobre o centro do objeto"));
+	centroButton->setChecked(true);
 	pontoButton = new QRadioButton(this);
 	pontoButton->setObjectName(QString::fromUtf8("pontoButton"));
-	pontoButton->setGeometry(QRect(40, 100, 291, 22));
+	pontoButton->setGeometry(QRect(40, 70, 281, 20));
 	pontoButton->setText(
 			QString::fromUtf8("Rotacionar sobre um ponto especifico"));
 	QLabel *anguloLabel = new QLabel(this);
-	anguloLabel->setGeometry(QRect(20, 140, 141, 20));
+	anguloLabel->setGeometry(QRect(20, 120, 141, 20));
 	anguloLabel->setText(QString::fromUtf8("Ângulo de rotação:"));
 	anguloInput = new QLineEdit(this);
-	anguloInput->setGeometry(QRect(30, 170, 60, 25));
+	anguloInput->setGeometry(QRect(30, 150, 60, 25));
 	QLabel *grausXLabel = new QLabel(this);
-	grausXLabel->setGeometry(QRect(90, 170, 15, 25));
+	grausXLabel->setGeometry(QRect(90, 150, 15, 25));
 	grausXLabel->setText(QString::fromUtf8("º"));
 	pontoLabel = new QLabel(this);
-	pontoLabel->setGeometry(QRect(20, 210, 100, 17));
+	pontoLabel->setGeometry(QRect(20, 190, 100, 17));
 	pontoLabel->setText(QString::fromUtf8("Ponto"));
-	pontoLabel->hide();
 	x1Label = new QLabel(this);
-	x1Label->setGeometry(QRect(20, 230, 10, 25));
+	x1Label->setGeometry(QRect(20, 210, 10, 25));
 	x1Label->setText(QString::fromUtf8("x"));
-	x1Label->hide();
 	y1Label = new QLabel(this);
-	y1Label->setGeometry(QRect(110, 230, 10, 25));
+	y1Label->setGeometry(QRect(110, 210, 10, 25));
 	y1Label->setText(QString::fromUtf8("y"));
-	y1Label->hide();
 	z1Label = new QLabel(this);
-	z1Label->setGeometry(QRect(200, 230, 10, 25));
+	z1Label->setGeometry(QRect(200, 210, 10, 25));
 	z1Label->setText(QString::fromUtf8("z"));
-	z1Label->hide();
 	x1Input = new QLineEdit(this);
-	x1Input->setGeometry(QRect(30, 230, 60, 25));
-	x1Input->hide();
+	x1Input->setGeometry(QRect(30, 210, 60, 25));
 	y1Input = new QLineEdit(this);
-	y1Input->setGeometry(QRect(120, 230, 60, 25));
-	y1Input->hide();
+	y1Input->setGeometry(QRect(120, 210, 60, 25));
 	z1Input = new QLineEdit(this);
-	z1Input->setGeometry(QRect(210, 230, 60, 25));
-	z1Input->hide();
+	z1Input->setGeometry(QRect(210, 210, 60, 25));
 	x2Label = new QLabel(this);
-	x2Label->setGeometry(QRect(20, 265, 10, 25));
+	x2Label->setGeometry(QRect(20, 245, 10, 25));
 	x2Label->setText(QString::fromUtf8("x"));
 	x2Label->hide();
 	y2Label = new QLabel(this);
-	y2Label->setGeometry(QRect(110, 265, 10, 25));
+	y2Label->setGeometry(QRect(110, 245, 10, 25));
 	y2Label->setText(QString::fromUtf8("y"));
 	y2Label->hide();
 	z2Label = new QLabel(this);
-	z2Label->setGeometry(QRect(200, 265, 10, 25));
+	z2Label->setGeometry(QRect(200, 245, 10, 25));
 	z2Label->setText(QString::fromUtf8("z"));
 	z2Label->hide();
 	x2Input = new QLineEdit(this);
-	x2Input->setGeometry(QRect(30, 265, 60, 25));
+	x2Input->setGeometry(QRect(30, 245, 60, 25));
 	x2Input->hide();
 	y2Input = new QLineEdit(this);
-	y2Input->setGeometry(QRect(120, 265, 60, 25));
+	y2Input->setGeometry(QRect(120, 245, 60, 25));
 	y2Input->hide();
 	z2Input = new QLineEdit(this);
-	z2Input->setGeometry(QRect(210, 265, 60, 25));
+	z2Input->setGeometry(QRect(210, 245, 60, 25));
 	z2Input->hide();
 	adicionarButton = new QPushButton(this);
 	adicionarButton->setObjectName(QString::fromUtf8("adicionarButton"));
@@ -96,9 +86,7 @@ RotacaoView::RotacaoView(OnAdicionarTipoTransformacaoEvent* event) :
 void RotacaoView::on_adicionarButton_clicked() {
 	Rotacao* rotacao = new Rotacao();
 	rotacao->angulo = strtod(this->anguloInput->text().toUtf8().data(), NULL);
-	if(origemButton->isChecked()){
-		rotacao->tipoRotacao = Rotacao::ORIGEM;
-	}else if(centroButton->isChecked()){
+	if(centroButton->isChecked()){
 		rotacao->tipoRotacao = Rotacao::CENTRO;
 		rotacao->setXFinal(strtod(this->x1Input->text().toUtf8().data(), NULL));
 		rotacao->setYFinal(strtod(this->y1Input->text().toUtf8().data(), NULL));
@@ -118,13 +106,6 @@ void RotacaoView::on_adicionarButton_clicked() {
 void RotacaoView::on_pontoButton_toggled(bool checked) {
 	if (checked) {
 		pontoLabel->setText(QString::fromUtf8("Pontos"));
-		pontoLabel->show();
-		x1Input->show();
-		y1Input->show();
-		z1Input->show();
-		x1Label->show();
-		y1Label->show();
-		z1Label->show();
 		x2Input->show();
 		y2Input->show();
 		z2Input->show();
@@ -132,13 +113,7 @@ void RotacaoView::on_pontoButton_toggled(bool checked) {
 		y2Label->show();
 		z2Label->show();
 	} else {
-		pontoLabel->hide();
-		x1Input->hide();
-		y1Input->hide();
-		z1Input->hide();
-		x1Label->hide();
-		y1Label->hide();
-		z1Label->hide();
+		pontoLabel->setText(QString::fromUtf8("Ponto"));
 		x2Input->hide();
 		y2Input->hide();
 		z2Input->hide();
@@ -148,34 +123,8 @@ void RotacaoView::on_pontoButton_toggled(bool checked) {
 	}
 }
 
-void RotacaoView::on_centroButton_toggled(bool checked) {
-	if (checked) {
-		pontoLabel->setText(QString::fromUtf8("Ponto final"));
-		pontoLabel->show();
-		x1Input->show();
-		y1Input->show();
-		z1Input->show();
-		x1Label->show();
-		y1Label->show();
-		z1Label->show();
-	} else {
-		pontoLabel->hide();
-		x1Input->hide();
-		y1Input->hide();
-		z1Input->hide();
-		x1Label->hide();
-		y1Label->hide();
-		z1Label->hide();
-	}
-}
-
-void RotacaoView::pontoVisibility(bool checked){
-
-}
-
 RotacaoView::~RotacaoView() {
 	delete frame;
-	delete origemButton;
 	delete centroButton;
 	delete pontoButton;
 	delete anguloInput;
--- src/utils/MatrizUtils.h
+++ src/utils/MatrizUtils.h
@@ -63,40 +63,42 @@ public:
 	}
 
 	static Matriz* getFullMatrizRotacao(Coordenada* ponto, Rotacao* rotacao){
-		MatrizRotacao* matrizRotacao = new MatrizRotacao(rotacao->angulo, rotacao->around);
-		if(rotacao->tipoRotacao != Rotacao::ORIGEM){
-			if(rotacao->tipoRotacao == Rotacao::CENTRO){
-				rotacao->setX(ponto->getX());
-				rotacao->setY(ponto->getY());
-				rotacao->setZ(ponto->getZ());
-			}
-			Coordenada* pontoFinal = rotacao->getVetor()->getCoordenadaFinal();
-			Matriz* transCenter = new MatrizTranslacao(-rotacao->getX(), -rotacao->getY(), -rotacao->getZ());
-			pontoFinal->multiplyByMatriz(transCenter);
-
-			double anguloXY = Utils::getAnguloPlanoXY(pontoFinal);
-			MatrizRotacao matrizXY(anguloXY, Rotacao::AROUND_X);
-			pontoFinal->multiplyByMatriz(&matrizXY);
-			transCenter->multiply(&matrizXY);
-
-			double anguloZY = Utils::getAnguloPlanoZY(pontoFinal);
-			MatrizRotacao matrizZY(anguloZY, Rotacao::AROUND_Z);
-			transCenter->multiply(&matrizZY);
-			transCenter->multiply(matrizRotacao);
-			pontoFinal->multiplyByMatriz(&matrizZY);
-			pontoFinal->print();
-
-			MatrizRotacao matrizZYBack(-anguloZY, Rotacao::AROUND_Z);
-			transCenter->multiply(&matrizZYBack);
-
-			MatrizRotacao matrizXYBack(-anguloXY, Rotacao::AROUND_X);
-			transCenter->multiply(&matrizXYBack);
-
-			MatrizTranslacao transCenterBack(rotacao->getX(), rotacao->getY(), rotacao->getZ());
-			transCenter->multiply(&transCenterBack);
-			delete matrizRotacao;
-			return transCenter;
+		MatrizRotacao* matrizRotacao = new MatrizRotacao(rotacao->angulo,
+				rotacao->around);
+		if (rotacao->tipoRotacao == Rotacao::CENTRO) {
+			rotacao->setX(ponto->getX());
+			rotacao->setY(ponto->getY());
+			rotacao->setZ(ponto->getZ());
 		}
+		Coordenada* pontoFinal = rotacao->getVetor()->getCoordenadaFinal();
+		Matriz* transCenter = new MatrizTranslacao(-rotacao->getX(),
+				-rotacao->getY(), -rotacao->getZ());
+		pontoFinal->multiplyByMatriz(transCenter);
+
+		double anguloXY = Utils::getAnguloPlanoXY(pontoFinal);
+		MatrizRotacao matrizXY(anguloXY, Rotacao::AROUND_X);
+		pontoFinal->multiplyByMatriz(&matrizXY);
+		transCenter->multiply(&matrizXY);
+
+		double anguloZY = Utils::getAnguloPlanoZY(pontoFinal);
+		MatrizRotacao matrizZY(anguloZY, Rotacao::AROUND_Z);
+		transCenter->multiply(&matrizZY);
+		transCenter->multiply(matrizRotacao);
+		pontoFinal->multiplyByMatriz(&matrizZY);
+		pontoFinal->print();
+
+		MatrizRotacao matrizZYBack(-anguloZY, Rotacao::AROUND_Z);
+		transCenter->multiply(&matrizZYBack);
+
+		MatrizRotacao matrizXYBack(-anguloXY, Rotacao::AROUND_X);
+		transCenter->multiply(&matrizXYBack);
+
+		MatrizTranslacao transCenterBack(rotacao->getX(), rotacao->getY(),
+				rotacao->getZ());
+		transCenter->multiply(&transCenterBack);
+		delete matrizRotacao;
+		return transCenter;
+
 		return matrizRotacao;
 	}
 
