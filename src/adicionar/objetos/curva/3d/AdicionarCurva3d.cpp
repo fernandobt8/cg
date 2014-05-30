aeafd93f575017969b9d225b0042da01204f6391
fernandobt8 <fernandobt8@gmail.com>
2014-05-30 00:25:03 -0300

    correcao adicionar curva 3d

--- src/adicionar/objetos/curva/3d/AdicionarCurva3d.cpp
+++ src/adicionar/objetos/curva/3d/AdicionarCurva3d.cpp
@@ -20,28 +20,24 @@ AdicionarCurva3d::~AdicionarCurva3d() {
 }
 
 void AdicionarCurva3d::on_okButton_clicked() {
-	if(coordenadasObjeto->size() == 16){
+	if(coordenadasObjeto->size() >= 16 && coordenadasObjeto->size()%8 == 0){
 		list<Coordenada*>* novaLista = new list<Coordenada*>();
 		list<Coordenada*>::iterator it = coordenadasObjeto->begin();
 		while (it != coordenadasObjeto->end()) {
 			novaLista->push_back((*it)->clone());
 			it++;
-	}
-	Curva3D *p = new Curva3D(Utils::cloneChar(this->nomeInput->text().toUtf8().data()), novaLista);
-	if(this->radioButton2->isChecked())
-		p->tipoCuva = Curva3D::SPLINES;
-	addEvent->OnAdicionarObjetoTipoClick(p);
+		}
+		Curva3D *p = new Curva3D(Utils::cloneChar(this->nomeInput->text().toUtf8().data()), novaLista);
+		if(this->radioButton2->isChecked())
+			p->tipoCuva = Curva3D::SPLINES;
+		addEvent->OnAdicionarObjetoTipoClick(p);
 	}else
-		QMessageBox::information(NULL, "Aviso", "Sao necessarios 16 pontos!");
+		QMessageBox::information(NULL, "Aviso", "Sao necessarios no mais de 16 pontos multiplo de 8!");
 }
 
 void AdicionarCurva3d::on_adicionarCoordenada_clicked() {
 	this->radioButton1->setEnabled(false);
 	this->radioButton2->setEnabled(false);
-	if(coordenadasObjeto->size() < 16){
-		AdicionarCurva::on_adicionarCoordenada_clicked();
-	}else
-		QMessageBox::information(NULL, "Aviso", "Ja foram adicionados 16 pontos!");
-
+	AdicionarCurva::on_adicionarCoordenada_clicked();
 }
 
